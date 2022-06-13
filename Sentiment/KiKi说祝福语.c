24ed1260091f72a75e00d88e7418c95e3c6701cc
chen <you@example.com>
2022-06-13 11:19:48 +0800

    添加项目文件。

---
+++ Sentiment/KiKi说祝福语.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int q = 0, w = 0;
+	scanf("%d", &q);
+	for (w = 0; w < q; w++)
+	{
+		printf("Happy new year!Good luck!\n");
+	}
+	return 0;
+}
